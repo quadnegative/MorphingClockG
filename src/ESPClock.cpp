b42e02f95bbfc8c2c8d20be52f2ceff07991c6b9
nathan wagner <wagner.452@gmail.com>
2024-01-15 16:54:41 -0500

    adjusted for wroom32 board

--- src/ESPClock.cpp
+++ src/ESPClock.cpp
@@ -43,6 +43,10 @@
   #include <Ticker.h>
   //#include <WiFi.h>
   #include <ESPmDNS.h>
+  #define P_A 19
+  #define P_B 21
+  #define P_C 4
+  #define P_D 5
   #define P_A 22
   #define P_B 21
   #define P_C 5
